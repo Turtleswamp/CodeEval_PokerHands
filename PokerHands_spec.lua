c0fa0ea458f75935a7bb7f1f671b15fa6340185f
Lucas Gerber <lgerber@lamotte.com>
2014-04-23 19:29:32 -0400

    began work on busted spec for solution

---
+++ PokerHands_spec.lua
@@ -0,0 +1,9 @@
+-- spec file for solution to poker hands CodeEval problem
+
+require("busted");
+
+describe("Test Hand clasifications", function()
+	it("is high card", function() 
+		assert.same(new_hand("2S 4D 7C TS JH"):get_value(), )
+		end)
+	end)
